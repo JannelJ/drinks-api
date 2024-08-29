5e685d6dfeafb9c9238ed3079816d41cbbfda65c
JannelJ <janneljones26@gmail.com>
2024-08-29 12:20:16 +0100

    wip: use request param

--- src/main/java/com/northcoders/drinksapi/controller/HomeController.java
+++ src/main/java/com/northcoders/drinksapi/controller/HomeController.java
@@ -1,9 +1,14 @@
 package com.northcoders.drinksapi.controller;
 
 
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping
@@ -17,6 +22,16 @@ public class HomeController {
     @GetMapping("/coffeelover")
     public String getCoffeeLover(){
         return "I love coffee!";
+    }
+
+    @GetMapping("/coffee")
+    public Map<String, String> coffee (@RequestParam(value = "name", defaultValue = "latte") String name) {
+        Map<String, String> expectedContent = new HashMap<>();
+        expectedContent.put("1", "id");
+        expectedContent.put("name", name);
+        return expectedContent;
+
+
     }
 
 }
--- src/test/java/controller/HomeControllerTests.java
+++ src/test/java/controller/HomeControllerTests.java
@@ -1,6 +1,8 @@
 package controller;
 
 import com.northcoders.drinksapi.controller.HomeController;
+import netscape.javascript.JSObject;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,6 +10,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
 
 @AutoConfigureMockMvc
 @SpringBootTest(classes = HomeController.class)
@@ -38,4 +46,22 @@ public class HomeControllerTests {
                 .andExpect(MockMvcResultMatchers.content().string(expectedContent));
     }
 
+    @Test
+    public void coffee() throws Exception {
+
+//       JSONObject expectedContent = new JSONObject();
+//        expectedContent.put("id", 1);
+//        expectedContent.put("name", "cappuccino");
+        Map<String, String> expectedContent = new HashMap<>();
+        expectedContent.put("1", "id");
+        expectedContent.put("name", "cappuccino");
+
+//        String expectedContent = "{\"1\":\"id\",\"name\":\"latte\"}";
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/coffee")
+                                .param("name", "cappuccino"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", is("cappuccino")).value(expectedContent));
+    }
 }
